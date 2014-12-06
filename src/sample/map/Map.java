a80970b68908c3a3e7dd6f39f1f95b73c45c9a20
akadatsky <andrey.kadatsky@gmail.com>
2014-12-06 10:32:54 +0200

    add getNeighbors()

--- src/sample/map/Map.java
+++ src/sample/map/Map.java
@@ -2,6 +2,7 @@ package sample.map;
 
 import sample.Const;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
@@ -29,8 +30,24 @@ public class Map {
 
     }
 
-    private List<Cell> getNeighbors(int i, int j) {
-        return null;
+    private List<Cell> getNeighbors(int x, int y) {
+        List<Cell> result = new ArrayList<Cell>();
+
+        if (x != 0) {
+            result.add(cells[x - 1][y]);
+        }
+        if (x != Const.ROW_SIZE - 1) {
+            result.add(cells[x + 1][y]);
+        }
+
+        if (y != 0) {
+            result.add(cells[x][y - 1]);
+        }
+        if (y != Const.ROW_COUNT - 1) {
+            result.add(cells[x][y + 1]);
+        }
+
+        return result;
     }
 
 }
